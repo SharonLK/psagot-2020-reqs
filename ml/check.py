dccb0420607c77086b7f77a5a6e01877f23173e8
Sharon Kuninin <schnitzelftw@gmail.com>
2020-08-10 10:37:49 +0300

    Add environments for ML and CV

---
+++ ml/check.py
@@ -0,0 +1,97 @@
+import pandas
+import glob
+import numpy
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from IPython.display import Image
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from collections import defaultdict
+from tqdm.autonotebook import tqdm
+
+import seaborn as sns
+import scipy.special
+from scipy import stats
+import matplotlib.pyplot as plt
+import itertools
+import holoviews as hv
+hv.extension('bokeh')
+
+from bokeh.plotting import figure, show
+
+import pickle as pkl
+import pydotplus
+import os
+import json
+
+from io import StringIO
+
+import pandas as pd
+from sklearn.tree import export_graphviz
+from IPython.display import display
+
+import seaborn as sns
+from scipy import stats
+import matplotlib.pyplot as plt
+import itertools
+import holoviews as hv
+hv.extension('bokeh')
+from bokeh.plotting import figure, show
+
+import pydotplus
+
+import pandas as pd
+import glob
+
+import numpy as np
+from sklearn.svm import SVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from IPython.display import Image
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix ,f1_score, make_scorer
+from collections import defaultdict
+from tqdm.autonotebook import tqdm
+from collections import Counter
+
+import seaborn as sns
+import scipy.special
+from scipy import stats
+import matplotlib.pyplot as plt
+import itertools
+import holoviews as hv
+from bokeh.plotting import figure, show
+
+import pickle as pkl
+import pydotplus
+import os
+import json
+import pandas as pd
+import glob
+
+import numpy as np
+from sklearn.svm import SVC, LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+import matplotlib.pyplot as plt
+from IPython.display import Image
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import precision_recall_fscore_support, confusion_matrix ,f1_score, make_scorer
+from collections import defaultdict
+from tqdm.autonotebook import tqdm
+from collections import Counter
+
+import seaborn as sns
+import scipy.special
+from scipy import stats
+import matplotlib.pyplot as plt
+import itertools
+import holoviews as hv
+from bokeh.plotting import figure, show
+
+import pickle as pkl
+import pydotplus
+import os
+import json
+
+print('yay')
