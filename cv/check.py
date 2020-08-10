e06e612105be77768bb71895c014c5a970d87473
Sharon Kuninin <schnitzelftw@gmail.com>
2020-08-10 11:30:20 +0300

    Add chceck file for CV

---
+++ cv/check.py
@@ -0,0 +1,52 @@
+import torch
+from PIL import Image, ImageDraw
+import matplotlib.pyplot as plt
+from torchvision import transforms as trans
+import numpy as np
+import cv2
+import os
+import json
+
+from torch.nn import Linear, Conv2d, BatchNorm1d, BatchNorm2d, PReLU, ReLU, Sigmoid, Dropout2d, Dropout, AvgPool2d, MaxPool2d, AdaptiveAvgPool2d, Sequential, Module, Parameter
+import torch.nn.functional as F
+import torch
+from collections import namedtuple
+import math
+import pdb
+
+from torch.nn import Linear, Conv2d, BatchNorm1d, BatchNorm2d, PReLU, ReLU, Sigmoid, Dropout2d, Dropout, AvgPool2d, MaxPool2d, AdaptiveAvgPool2d, Sequential, Module, Parameter
+import torch.nn.functional as F
+import torch
+from collections import namedtuple
+import math
+import pdb
+
+import numpy as np
+import torch
+from PIL import Image
+from torch.autograd import Variable
+
+import numpy as np
+import torch
+from PIL import Image
+from torch.autograd import Variable
+
+from PIL import Image
+
+from PIL import Image
+import cv2
+import numpy as np
+import mxnet as mx
+import io
+from pathlib import Path
+
+from PIL import Image
+import cv2
+import numpy as np
+
+import numpy as np
+import cv2
+
+import numpy as np
+import torch
+from torch.autograd import Variable
