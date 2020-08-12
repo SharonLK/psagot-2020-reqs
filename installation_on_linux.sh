f8a98455aa4039b988b0ba836a4f2c05e8eda1a8
Sharon Kuninin <schnitzelftw@gmail.com>
2020-08-12 20:03:07 +0300

    Add revamped linux installation script

---
+++ installation_on_linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Update apt
+sudo apt update
+
+# Install utilities.
+
+sudo apt install vim -y
+sudo apt install sl -y
+
+# Install networking tools.
+sudo apt install net-tools -y
+sudo apt install wireshark -y
+
+# Install Python software
+sudo apt install ipython3 -y
+sudo apt install python3-pip -y
